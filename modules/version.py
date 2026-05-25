f6ad17c7ff58097e6acbd03f24cd516646bcce9e
Matthew Behrens <mattbehrens02@gmail.com>
2026-05-25 12:04:30 -0600

    chore: bump app version to 26.1.2

--- modules/version.py
+++ modules/version.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class AppVersion:
     app_name: str = "Secure Wipe"
-    app_version: str = "26.1.1" # year.major.minor
+    app_version: str = "26.1.2" # year.major.minor
     recovery_schema_version: str = "1.1" # major.minor
     report_schema_version: str = "1.1" # major.minor
